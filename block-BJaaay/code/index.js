814e311a02c263c0123c9acdd2c0d0989fcb8a82
Arun <akr7205@gmail.com>
2022-03-24 10:30:37 +0530

    1st commit

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,41 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for(let i=1;i<=10;i++)
+{
+ console.log(i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
-
+for(let i=1;i<=10;i++)
+{
+ if(i%2==0)
+ {
+    console.log(i);
+ }
+ 
+}
 // Using `console.log` log all the od values from 1 to 10.
-
+for(let i=1;i<=10;i++)
+{
+ if(i%2!=0)
+ {
+    console.log(i);
+ }
+ 
+}
 // Calculate the sum of all numbers from 1 to 10.
+let sum=0;
+for(let i=1;i<=10;i++)
+{
+ sum=sum+i;
+}
+console.log(sum);
 
 // Log all the values from 1 to 10 using while loop
+let j=1;
+while(j<=10)
+{
+    console.log(j);
+    j++;
+}
